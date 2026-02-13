4e2556579bf951fabbd5696c62a139e5407a2f8d
Timmmich19 <t.akhmatovya@yandex.ru>
2026-02-13 13:40:42 +0500

    Added routes to home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="min-h-screen bg-background text-foreground flex justify-center py-4">
+    <div class="w-full px-4">
+      <Card>
+        <CardHeader class="flex flex-row items-center gap-4">
+          <div>
+            <CardTitle class="text-2xl"> Routes: </CardTitle>
+          </div>
+        </CardHeader>
+
+        <CardContent class="space-y-6">
+          <RouterLink to="/saasoftform" class="text-blue-500 hover:underline">
+            Go to Accounts Layout
+          </RouterLink>
+        </CardContent>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
