77b308d0ec169353764a633399298c77bc636695
Timmmich19 <t.akhmatovya@yandex.ru>
2026-02-13 13:40:42 +0500

    Added routes to home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccountsLayout from '@/components/saasoftform/AccountsLayout.vue'
+import HomePage from '@/components/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/saasoftform', component: AccountsLayout }],
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/saasoftform', component: AccountsLayout },
+  ],
 })
 
 export default router
